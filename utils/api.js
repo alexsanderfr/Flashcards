4d2a799aac6c03bcf5fd98c1f952409284b4fc70
alexsanderfr <alexsanderfrancorosa@gmail.com>
2019-05-20 22:17:51 -0300

    feat: Add api

---
+++ utils/api.js
@@ -0,0 +1,30 @@
+import { AsyncStorage } from 'react-native'
+
+const DECK_STORAGE_KEY = 'Flashcards:deck'
+
+export function getDecks(id) {
+    return AsyncStorage.getItem(DECK_STORAGE_KEY)
+        .then((results) => results[id])
+}
+
+export function getDeck() {
+    return AsyncStorage.getItem(DECK_STORAGE_KEY)
+}
+
+export function saveDeckTitle(title) {
+    return AsyncStorage.mergeItem(DECK_STORAGE_KEY, JSON.stringify({
+        [title]: {
+            title: title,
+            questions: []
+        }
+      }))
+}
+
+export function addCardToDeck(title, card) {
+    return AsyncStorage.mergeItem(DECK_STORAGE_KEY, JSON.stringify({
+        [title]: {
+            title: title,
+            questions: [card]
+        }
+      }))
+}
