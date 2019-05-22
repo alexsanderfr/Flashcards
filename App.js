2523fc12fd927556314ce3588c09be4fa579f639
alexsanderfr <alexsanderfrancorosa@gmail.com>
2019-05-21 22:20:05 -0300

    feat: Add createStore

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={createStore(reducer)}>
+        <View style={styles.container}>
+          <Text>Hello, World!</Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
